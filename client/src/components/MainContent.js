b2392d8a40b84c322dbb308d944a363c53ec4a86
Harsh Garg <gargharsh2512@gmail.com>
2023-04-16 14:23:33 +0530

    Change fetch URL in client

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -8,7 +8,7 @@ const MainContent = () => {
   const [constants, setConstants] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/constants", {
+    fetch("https://physicssem2pbl.up.railway.app/api/constants", {
       "Content-Type": "application/json",
     })
       .then((res) => res.json())
